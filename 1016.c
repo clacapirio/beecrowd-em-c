946fad77ed80439e242ab0f2a95dd41cf12bff4f
Claudio Moises da Silva <clacapirio@gmail.com>
2024-10-15 20:31:56 -0300

    Create 1016.c

---
+++ 1016.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    
+	int minutos;
+	
+	scanf("%d",&minutos);
+	
+	printf("%d minutos\n",minutos*2);
+	return 0;
+}
+
+\\o certo seria outra fórmula, mas não estava conseguindo nessa época pois ainda estava iniciando na programação
